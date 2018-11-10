2e22f9f9d6487ffc36e64a0fb5bae8c7878819f7
Danny McCaslin <mccaslin.danny@gmail.com>
2018-11-10 13:47:56 -0500

    Added a list of open ports at the end of the running program, and if they are associated with popular protocols it losts those protocols.

--- PyportScanner.py
+++ PyportScanner.py
@@ -18,21 +18,24 @@ remoteServer = input("Enter a remote host: ")
 remoteServerIP = socket.gethostbyname(remoteServer)
 
 #Print a Banner
-print("-")*60
+print("-",60)
 print("Please wait, scanning remote host " + remoteServerIP)
-print("-")*60
+print("-",60)
 
 #Check what time the scan started
 t1 = datetime.now()
 
 #Using range function, specify ports
+successes = []
 
 try:
     for port in range(1,1025):
         sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         result = sock.connect_ex((remoteServerIP,port))
+        print("Trying port ",port)
         if result == 0:
             print("Port{}: Open".format(port))
+            successes.append(port)
         sock.close()
 
 except KeyboardInterrupt:
@@ -49,9 +52,15 @@ except sock.error:
 
 # Checking the time again
 t2 = datetime.now()
-
+# Create a dictionary of popular ports
+popports = {22:'ssh',23:'telnet',25:'smtp',53:'dns',67:'dhcp',69:'tftp',
+            80:'http',110:'pop3',143:'imap',389:'ldap',443:'https'}
 # Calculates the difference of time, to see how long it took to run the script
 total =  t2 - t1
 
 # Printing the information to screen
 print('Scanning Completed in: ', total)
+print('Open Ports: ')
+for i in range(len(successes)):
+    print(successes[i],':',popports[successes[i]])
+
